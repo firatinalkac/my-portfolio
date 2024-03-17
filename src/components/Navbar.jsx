db82de3450fb084426a7c2f30b99b95cd94dbdb8
firat.inalkac <firat.inalkac@tesodev.com>
2024-03-18 00:32:34 +0300

    first commit: setup project and added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+"use client"
+import Link from "next/link"
+import Image from "next/image"
+import { useState } from "react"
+import NavLink from "./NavLink"
+
+const links = [
+  {url: '/', title: 'Home'},
+  {url: '/about', title: 'About'},
+  {url: '/portfolio', title: 'Portfolio'},
+  {url: '/contact', title: 'Contact'}
+]
+
+const socialMediaAccounts = [
+  {url: "github", image: 'github-light-icon'},
+  {url: "linkedin", image: 'linkedin-light-icon'},
+]
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xlg:px-28 '>
+      {/* LINKS */}
+      <div className="hidden md:flex gap-3 w-1/3">
+        {links.map((link) => <NavLink link={link} key={link.title} /> )}
+      </div>
+      {/* LINKS */}
+      {/* LOGO */}
+      <div className="w-1/3 flex justify-center">
+        <Link href="/" className="text-sm bg-black rounded-md p-1">
+          <span className='text-white'>firatinlkc</span>
+          <span className="text-black bg-white rounded ml-1 px-1">.dev</span>
+        </Link>
+      </div>
+      {/* LOGO */}
+      {/* Social Media */}
+      <div className="hidden md:flex gap-4 w-1/3 justify-end">
+        {
+          socialMediaAccounts.map((account, index) => 
+            <Link href={account.url} key={index}>
+              <Image src={`${account.image}.svg`} width={20} height={20} alt={account.url} />
+            </Link>
+          )
+        }
+      </div>
+      {/* Social Media */}
+      {/* Responsive Menu */}
+      <div className="md:hidden">
+        <button className="w-8 h-6 flex flex-col justify-between z-50 relative" onClick={() => setOpen((prev) => !prev)}>
+          <div className="h-1 w-full bg-white rounded"></div>
+          <div className="h-1 w-full bg-white rounded"></div>
+          <div className="h-1 w-full bg-white rounded"></div>
+        </button>
+        {/* MENU LIST */}
+        {open && <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+          {
+            links.map((link) => {
+              return (
+                <Link href={link.url} key={link.title}>{link.title}</Link>
+              )
+            })
+          }
+        </div>}
+      </div>
+    </div>
+  )
+}
