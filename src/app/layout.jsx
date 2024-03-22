115788c470a337e19877ba7762d36fa7a5d6bcae
firat.inalkac <firat.inalkac@tesodev.com>
2024-03-22 09:07:31 +0300

    Animation has been added to page transitions.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,24 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import TransitionProvider from "@/components/TransitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "firatinlkc.dev",
-  description: "Everythings is code",
+  description: "Everything is code",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-50 to-red-50">
-          <div className="h-20">
-            <Navbar />
-          </div>
-          <div className="h-[calc(100vh-6rem)] px-4 sm:px-8 md:px-12 lg:px-20 xlg:px-28">{children}</div>
-        </div>
+        <TransitionProvider>{children}</TransitionProvider>
       </body>
     </html>
   );
